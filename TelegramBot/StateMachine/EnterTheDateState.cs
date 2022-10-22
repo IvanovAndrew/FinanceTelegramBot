9bef5b3a941bbef7f17b8bfed7a4ea8d3e6adf3f
warrior_spb <ivanovandrew2004@gmail.com>
2022-10-22 12:00:56 +0400

    Update logic related to asking date

--- TelegramBot/StateMachine/EnterTheDateState.cs
+++ TelegramBot/StateMachine/EnterTheDateState.cs
@@ -7,6 +7,7 @@ using GoogleSheet;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot.StateMachine;
 
@@ -16,37 +17,56 @@ class EnterTheDateState : IExpenseInfoState
     private readonly IEnumerable<Category> _categories;
     private readonly GoogleSheetWriter _spreadsheetWriter;
     private readonly ILogger _logger;
+    private readonly bool _askCustomDate; 
     
-    public EnterTheDateState(IEnumerable<Category> categories, IMoneyParser moneyParser, GoogleSheetWriter spreadsheetWriter, ILogger logger)
+    public EnterTheDateState(IEnumerable<Category> categories, IMoneyParser moneyParser, GoogleSheetWriter spreadsheetWriter, ILogger logger, bool askCustomDate = false)
     {
         _categories = categories;
         _spreadsheetWriter = spreadsheetWriter;
         _moneyParser = moneyParser;
+        _askCustomDate = askCustomDate;
         _logger = logger;
     }
 
     public async Task<Message> Request(ITelegramBotClient botClient, long chatId, CancellationToken cancellationToken)
     {
-        return await botClient.SendTextMessageAsync(chatId: chatId, "Enter the date:", cancellationToken:cancellationToken);
+        var info = "Enter the date";
+
+        if (_askCustomDate)
+        {
+            return await botClient.SendTextMessageAsync(chatId, info, cancellationToken: cancellationToken);
+        }
+        
+        // TODO fix this
+        DateTime today = DateTime.Today;
+        InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+            // keyboard
+            new[]
+            {
+                // first row
+                InlineKeyboardButton.WithCallbackData(text:"Today", callbackData:today.ToString("dd.MM.yyyy")),
+                InlineKeyboardButton.WithCallbackData(text:"Yesterday", callbackData:today.ToString("dd.MM.yyyy")),
+                InlineKeyboardButton.WithCallbackData(text:"Other", callbackData:"Other"),
+            });
+        
+        return await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: info,
+            replyMarkup: inlineKeyboard,
+            cancellationToken: cancellationToken);
     }
 
     public IExpenseInfoState Handle(string text, CancellationToken cancellationToken)
     {
         var expenseBuilder = new ExpenseBuilder();
         DateOnly date;
+
+        if (text.ToLowerInvariant() == "other")
+        {
+            return new EnterTheDateState(_categories, _moneyParser, _spreadsheetWriter, _logger, true);
+        }
         
-        if (string.Equals("today", text, StringComparison.InvariantCultureIgnoreCase) ||
-            string.Equals("сегодня", text, StringComparison.InvariantCultureIgnoreCase))
-        {
-            date = new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
-        }
-        else if (string.Equals("yesterday", text, StringComparison.InvariantCultureIgnoreCase) ||
-                 string.Equals("вчера", text, StringComparison.InvariantCultureIgnoreCase))
-        {
-            var dateTime = DateTime.Now.AddDays(-1);
-            date = new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
-        }
-        else if (!DateOnly.TryParse(text, out date))
+        if (!DateOnly.TryParse(text, out date))
         {
             _logger.LogDebug($"{text} isn't a date");
             return new ErrorWithRetry($"{text} isn't a date.", this);
