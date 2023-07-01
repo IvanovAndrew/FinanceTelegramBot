7d6a68dc6af00e81a983a6bc2ad55d6865826bcd
warrior_spb <ivanovandrew2004@gmail.com>
2023-07-01 17:22:29 +0400

    Fix broken build

--- Infrastructure/DateOnlyExtension.cs
+++ Infrastructure/DateOnlyExtension.cs
@@ -1,15 +1,15 @@
 ﻿namespace Infrastructure;
 
-static class DateOnlyExtension
+public static class DateOnlyExtension
 {
-    internal static DateOnly LastDayOfMonth(this DateOnly date)
+    public static DateOnly LastDayOfMonth(this DateOnly date)
     {
         var lastDayOfMonth = new [] {0, 31, date.Year % 4 == 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         return new DateOnly(date.Year, date.Month, lastDayOfMonth[date.Month]);
     }
-        
-    internal static DateOnly FirstDayOfMonth(this DateOnly date)
+
+    public static DateOnly FirstDayOfMonth(this DateOnly date)
     {
         return new DateOnly(date.Year, date.Month, 1);
     }
