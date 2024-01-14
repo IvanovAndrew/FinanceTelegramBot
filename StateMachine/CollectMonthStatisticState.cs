9896f555e9bbf0c8ac6243050691434eb79ae367
warrior_spb <ivanovandrew2004@gmail.com>
2024-01-14 15:17:02 +0400

    Code clean up

---
+++ StateMachine/CollectMonthStatisticState.cs
@@ -0,0 +1,63 @@
+﻿using Domain;
+using Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace StateMachine;
+
+internal class CollectMonthStatisticState : IExpenseInfoState
+{
+    private readonly StateFactory _factory;
+    private readonly DateOnly _today;
+    private readonly ILogger<StateFactory> _logger;
+    private DatePickerState _datePicker; 
+    private const string DateFormat = "MMMM yyyy";
+
+    public IExpenseInfoState PreviousState { get; }
+    
+    public CollectMonthStatisticState(StateFactory factory, IExpenseInfoState previousState, DateOnly today, ILogger<StateFactory> logger)
+    {
+        _factory = factory;
+        PreviousState = previousState;
+        _today = today;
+        
+        _datePicker = new DatePickerState(this, "Enter the month", _today, DateFormat, 
+            new[] { _today, _today.AddMonths(-1) }, "Another month");
+        _logger = logger;
+    }
+
+    public bool UserAnswerIsRequired => true;
+    
+    public Task<IMessage> Request(ITelegramBot botClient, long chatId, CancellationToken cancellationToken = default)
+    {
+        return _datePicker.Request(botClient, chatId, cancellationToken);
+    }
+
+    public Task Handle(IMessage message, CancellationToken cancellationToken)
+    {
+        return Task.Run(() => { });
+    }
+
+    public IExpenseInfoState ToNextState(IMessage message, CancellationToken cancellationToken)
+    {
+        var nextState = _datePicker.ToNextState(message, cancellationToken);
+
+        if (nextState is DatePickerState datePickerState)
+        {
+            _datePicker = datePickerState;
+            return this;
+        }
+        
+        if (DateOnly.TryParseExact(message.Text, DateFormat, out var selectedMonth))
+        {
+            var expenseAggregator = new ExpensesAggregator<string>(e => e.Category, true, sortAsc:false);
+
+            var specification =
+                new ExpenseFromDateRangeSpecification(selectedMonth.FirstDayOfMonth(), selectedMonth.LastDayOfMonth());
+        
+            return _factory.GetExpensesState(this, specification, expenseAggregator, s => s,
+                new TableOptions(){Title = selectedMonth.ToString(DateFormat), FirstColumnName = "Category"});
+        }
+
+        return this;
+    }
+}
---
+++ StateMachine/DatePickerState.cs
@@ -0,0 +1,82 @@
+﻿using Infrastructure;
+
+namespace StateMachine;
+
+internal class DatePickerState : IExpenseInfoState
+{
+    protected readonly string Text;
+    protected readonly DateOnly Today;
+    protected readonly string DateFormat;
+    private readonly DateOnly[] _options;
+    private readonly string _customOptionTitle;
+    private const string CallbackCustom = "custom";
+
+    internal DatePickerState(IExpenseInfoState previousState, string text, DateOnly today, string dateFormat, DateOnly[] options, string customOptionTitle) : this(text, today, dateFormat)
+    {
+        PreviousState = previousState;
+        Text = text;
+        DateFormat = dateFormat;
+        _options = options;
+        _customOptionTitle = customOptionTitle;
+    }
+    
+    protected DatePickerState(string text, DateOnly today, string dateFormat)
+    {
+        Text = text;
+        Today = today;
+        DateFormat = dateFormat;
+    }
+    
+    public bool UserAnswerIsRequired => true;
+    public IExpenseInfoState PreviousState { get; }
+
+    
+    public virtual async Task<IMessage> Request(ITelegramBot botClient, long chatId, CancellationToken cancellationToken = default)
+    {
+        var buttons = new TelegramButton[_options.Length + 1];
+
+        for (int i = 0; i < _options.Length; i++)
+        {
+            var dateString = _options[i].ToString(DateFormat);
+            buttons[i] = new TelegramButton() { Text = dateString, CallbackData = dateString };
+        }
+
+        buttons[_options.Length] = new TelegramButton { Text = _customOptionTitle, CallbackData = CallbackCustom }; 
+        
+        var keyboard = TelegramKeyboard.FromButtons(buttons, chunkSize: 3);
+        
+        return await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: Text,
+            keyboard: keyboard,
+            cancellationToken: cancellationToken);
+    }
+
+    public Task Handle(IMessage message, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(0);
+    }
+
+    public IExpenseInfoState ToNextState(IMessage message, CancellationToken cancellationToken)
+    {
+        if (message.Text == CallbackCustom)
+        {
+            return new CustomDatePickerState(Text, Today, DateFormat);
+        }
+
+        return PreviousState;
+    }
+}
+
+internal class CustomDatePickerState : DatePickerState
+{
+    protected internal CustomDatePickerState(string text, DateOnly today, string dateFormat) : base(text, today, dateFormat)
+    {
+    }
+
+    public override async Task<IMessage> Request(ITelegramBot botClient, long chatId, CancellationToken cancellationToken)
+    {
+        return await botClient.SendTextMessageAsync(
+            chatId: chatId, $"{Text}. Example: {Today.ToString(DateFormat)}", cancellationToken:cancellationToken);
+    }
+}
