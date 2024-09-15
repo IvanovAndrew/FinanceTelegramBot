1a658676651469c412697e177441c6b6e1284bd9
warrior_spb <ivanovandrew2004@gmail.com>
2024-09-15 12:33:57 +0400

    Add a missing file

---
+++ GoogleSpreadsheet/AzureQueueTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace GoogleSpreadsheet;
+
+public class AzureQueueTrigger
+{
+    private readonly ILogger<AzureQueueTrigger> _logger;
+
+    public AzureQueueTrigger(ILogger<AzureQueueTrigger> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(AzureQueueTrigger))]
+    public void Run([QueueTrigger("save-expense")] QueueMessage message)
+    {
+        _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+    }
+}
