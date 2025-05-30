cfa2a557f4713cd97061683a3af75925669d7c03
warrior_spb <ivanovandrew2004@gmail.com>
2025-05-31 01:43:25 +0400

    Fix merge errors #48

--- Application/Statistic/StatisticBalance/StatisticBalanceQuestionnaireCreatedHandler.cs
+++ Application/Statistic/StatisticBalance/StatisticBalanceQuestionnaireCreatedHandler.cs
@@ -8,14 +8,25 @@ public class StatisticBalanceQuestionnaireCreatedHandler(IMessageService message
     {
         var today = dateTimeService.Today();
         
+        var options = new List<DateOnly> { today, today.AddMonths(-1) };
+
+        switch (today.Month)
+        {
+            case <= 2:
+                options.Add(new(today.Year - 1, 1, 1));
+                break;
+            default:
+                options.Add(new(today.Year, 1, 1));
+                break;
+        }
+        
         await messageService.EditSentTextMessageAsync(
             new Message()
             {
                 ChatId = notification.SessionId,
                 Id = notification.LastSentMessageId,
                 Text = "Enter the month",
-                Options = MessageOptions.FromListAndLastSingleLine(new []{today.ToString("MMMM yyyy"), today.AddMonths(-1).ToString("MMMM yyyy"), today.AddMonths(-6).ToString("MMMM yyyy")}, "Another month")
-            }
-        );
+                Options = MessageOptions.FromListAndLastSingleLine(options.Select(d => d.ToString("MMMM yyyy")).ToArray(), "Another month")
+            }, cancellationToken);
     }
 }
